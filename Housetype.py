f0cb323076609ccf9bcd21fb11ad36d34d087ed7
Saikumar Sannidhi <saikumar.sannidhi@walmart.com>
2022-11-12 23:00:51 -0800

    fixing api and including housetype lookup

--- Housetype.py
+++ Housetype.py
@@ -1,14 +1,41 @@
 from google.cloud import bigquery
-import json
+from google.oauth2 import service_account
+from flask import request
+credentials = service_account.Credentials.from_service_account_file('data298-347103-eaa0e3e59658.json')
 
-client = bigquery.Client()
+import json
+project_id = 'data298-347103'
+client = bigquery.Client(credentials= credentials,project=project_id)
 
 
 class Housetype:
     def get(self):
-        housetype_sql = "SELECT * FROM `data298-347103.County_Addresses.COUNTY_ADDRESS_DATA` LIMIT 10"
+        args = request.args
+        
+        #args = { 'address': '1644 STEMEL WAY', 'city': 'Milpitas','state': 'California','zipcode': '95035','family-size': '3','energy-bill': '0'}
+        print(args)
+        address = args['address']
+        city = args['city']
+        state = args['state']
+        zipcode = args['zipcode']
+        housetype_sql = """SELECT * FROM `data298-347103.County_Addresses.COUNTY_ADDRESS_DATA` 
+                            WHERE Address=@address and ZipCode=@zipcode and City=@city LIMIT 1"""
 
-        query_housetype = client.query(housetype_sql) 
-        housetype_records = [dict(row) for row in query_housetype]
-        json_obj = json.dumps(str(housetype_records))
-        return json_obj
+
+        job_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("address", "STRING", address),
+                bigquery.ScalarQueryParameter("city", "STRING", city),
+                bigquery.ScalarQueryParameter("zipcode", "STRING", zipcode),
+            ]
+        )
+        
+        housetype_result = client.query(housetype_sql,job_config=job_config) 
+        address_result = '{"result":"Address Not Valid"}'
+        if(housetype_result.result().total_rows>0):
+            housetype_records = [dict(row) for row in housetype_result]
+            address_result = json.dumps(str(housetype_records))
+
+        return address_result
+
+
--- main.py
+++ main.py
@@ -1,37 +1,30 @@
-from flask import Flask
+
+
+from flask import Flask, render_template
+import json
+app = Flask(__name__)
+
 from Users import Users
 from Locations import Locations
+from Housetype import Housetype
 
-class FlaskAppWrapper(object):
+@app.route('/users')
+def users():
+    user_obj = Users()
+    result = user_obj.get()
+    return result
 
-    def __init__(self, app, **configs):
-        self.app = app
-        self.configs(**configs)
+@app.route('/locations')
+def locations():
+    location_obj = Locations()
+    result = location_obj.get()
+    return result
 
-    def configs(self, **configs):
-        for config, value in configs:
-            self.app.config[config.upper()] = value
+@app.route('/housetype')
+def housetype():
+    housetype_obj = Housetype()
+    result = housetype_obj.get()
+    return result
 
-    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None, methods=['GET'], *args, **kwargs):
-        self.app.add_url_rule(endpoint, endpoint_name, handler, methods=methods, *args, **kwargs)
-
-    def run(self, **kwargs):
-        self.app.run(**kwargs)
-
-flask_app = Flask(__name__)
-
-app = FlaskAppWrapper(flask_app)
-
-def action():
-    """ Function which is triggered in flask app """
-    return "Hello World"
-
-user_obj = Users()
-locations_obj = Locations()
-# Add endpoint for the action function
-app.add_endpoint('/users', 'users', user_obj.get, methods=['GET'])
-app.add_endpoint('/locations', 'locations', locations_obj.get, methods=['GET'])
-app.add_endpoint('/action', 'action', action, methods=['GET'])
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
