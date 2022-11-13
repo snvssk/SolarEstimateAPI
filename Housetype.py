df0ed8e2bd0a5bab0fa9fd5fe464bdd881a86038
Saikumar Sannidhi <saikumar.sannidhi@walmart.com>
2022-11-13 01:51:45 -0800

    Adding mapbox api lookup , google maps api and Solar energy vertex api end point lookup

--- Housetype.py
+++ Housetype.py
@@ -1,14 +1,29 @@
 from google.cloud import bigquery
+from google.cloud import storage
 from google.oauth2 import service_account
 from flask import request
+import requests,json
+import time
+
+#Credentials
 credentials = service_account.Credentials.from_service_account_file('data298-347103-eaa0e3e59658.json')
+f = open ('maps_api_key.json', "r")
+maps_key_json = json.loads(f.read())
+google_maps_api_key = maps_key_json['google_maps_api_key']
+map_box_api_key = maps_key_json['map_box_api_key']
+
+GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+MAP_BOX_API_URL = 'https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/'
 
 import json
 project_id = 'data298-347103'
 client = bigquery.Client(credentials= credentials,project=project_id)
+storage_client = storage.Client(credentials=credentials, project=project_id)
 
 
 class Housetype:
+
+    #Fetch House type infromation from County Database
     def get(self):
         args = request.args
         
@@ -34,8 +49,60 @@ class Housetype:
         address_result = '{"result":"Address Not Valid"}'
         if(housetype_result.result().total_rows>0):
             housetype_records = [dict(row) for row in housetype_result]
+            latitude,longitude = self.latlong(address_string=address+city+state+zipcode)
+            housetype_records[0]['longitude'] = longitude
+            housetype_records[0]['latitude'] = latitude
             address_result = json.dumps(str(housetype_records))
-
+            self.mapbox(longitude,latitude)
         return address_result
+
+    #Fetch Latitude and Longitude 
+    def latlong(self,address_string):
+        print(address_string)
+        params = {
+            'address': address_string,
+            'sensor': 'false',
+            'region': 'india',
+            'key': google_maps_api_key 
+        }
+
+        # Do the request and get the response data
+        req = requests.get(GOOGLE_MAPS_API_URL, params=params)
+        res = req.json()
+
+        print(res)
+        # Use the first result
+        result = res['results'][0]
+
+        geodata = dict()
+        geodata['lat'] = result['geometry']['location']['lat']
+        geodata['lng'] = result['geometry']['location']['lng']
+        geodata['address'] = result['formatted_address']
+        print('{address}. (lat, lng) = ({lat}, {lng})'.format(**geodata))
+        return geodata['lat'],geodata['lng']
+        
+    def mapbox(self,longitude,latitude):
+        zoom_level = '19'
+        bearing = '47' # rotating map
+        pitch = '0' # tilting map
+        img_size = '256x256'
+        static_params = [str(longitude),str(latitude),zoom_level,bearing,pitch]
+        params = {
+            'access_token': map_box_api_key 
+        }
+        
+        # Do the request and get the response data
+        req = requests.get(MAP_BOX_API_URL+",".join(static_params)+"/"+img_size, params=params)
+        mapbox_downloaded_filename = "satellite_image_"+str(time.time())+".png"
+        local_folder_name = "mapbox_images/"
+        file = open(local_folder_name +mapbox_downloaded_filename, "wb")
+        file.write(req.content)
+        file.close()
+        bucket = storage_client.get_bucket('solarestimation_images')
+        blob = bucket.blob('mapbox_downloads/'+mapbox_downloaded_filename)
+        blob.upload_from_filename(local_folder_name +mapbox_downloaded_filename)
+        return 0
+       
+
 
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 from Users import Users
 from Locations import Locations
 from Housetype import Housetype
+from Solarenergy import Solarenergy
 
 @app.route('/users')
 def users():
@@ -24,6 +25,12 @@ def locations():
 def housetype():
     housetype_obj = Housetype()
     result = housetype_obj.get()
+    return result
+
+@app.route('/solarenergy')
+def solarenergy():
+    solarenergy_obj = Solarenergy()
+    result = solarenergy_obj.get()
     return result
 
 if __name__ == '__main__':
